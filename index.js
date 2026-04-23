9808b8d0c0f23cf9b7963e2ce0cc2734b239d6a5
denxxila <denxxilaa@gmail.com>
2026-04-23 18:14:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body><h1>Hello World</h1></body></html>');
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
-
-module.exports = server;
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send('<html><body><h1>Hello World</h1></body></html>');
+};
